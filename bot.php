08c87c39f657da8e70b46f93930028f7280a0da1
valzargaming <valzargaming@gmail.com>
2022-06-09 20:33:59 -0400

    owner_id

--- bot.php
+++ bot.php
@@ -246,8 +246,9 @@ $on_ready = function () use ($discord, $timer_function)
     }
 };
 
-$on_message = function ($message) use ($discord, $loop, $admiral, $captain, $knight, $veteran, $infantry, $insults_path, $nomads_discord2ooc, $tdm_discord2ooc, $nomads_discord2admin, $tdm_discord2admin, $nomads_discord2dm, $tdm_discord2dm, $nomads_discord2ban, $tdm_discord2ban, $nomads_discord2unban, $tdm_discord2unban, $nomads_whitelist, $tdm_whitelist, $nomads_bans, $tdm_bans, $nomads_updateserverabspaths, $nomads_serverdata, $nomads_dmb, $nomads_killsudos, $nomads_killciv13, $nomads_mapswap, $tdm_mapswap, $tdm_updateserverabspaths, $tdm_serverdata, $tdm_dmb, $tdm_killsudos, $tdm_killciv13, $nomads_ip, $nomads_port, $tdm_ip, $tdm_port, $command_symbol)
+$on_message = function ($message) use ($discord, $loop, $owner_id, $admiral, $captain, $knight, $veteran, $infantry, $insults_path, $nomads_discord2ooc, $tdm_discord2ooc, $nomads_discord2admin, $tdm_discord2admin, $nomads_discord2dm, $tdm_discord2dm, $nomads_discord2ban, $tdm_discord2ban, $nomads_discord2unban, $tdm_discord2unban, $nomads_whitelist, $tdm_whitelist, $nomads_bans, $tdm_bans, $nomads_updateserverabspaths, $nomads_serverdata, $nomads_dmb, $nomads_killsudos, $nomads_killciv13, $nomads_mapswap, $tdm_mapswap, $tdm_updateserverabspaths, $tdm_serverdata, $tdm_dmb, $tdm_killsudos, $tdm_killciv13, $nomads_ip, $nomads_port, $tdm_ip, $tdm_port, $command_symbol)
 {
+    if ($message->guild->owner_id != $owner_id) return; //Only process commands from a guild that Taislin owns
     if (!$command_symbol) $command_symbol = '!s';
     
     $author_user = $message->author; //This will need to be updated in a future release of DiscordPHP
@@ -937,8 +938,9 @@ $recalculate_ranking = function () use ($tdm_awards_path, $ranking_path)
     fclose ($search);
 };
 
-$on_message2 = function ($message) use ($discord, $loop, $recalculate_ranking, $ranking_path, $tdm_awards_path, $tdm_awards_br_path, $typespess_path, $typespess_launch_server_path, $command_symbol)
+$on_message2 = function ($message) use ($discord, $loop, $recalculate_ranking, $owner_id, $ranking_path, $tdm_awards_path, $tdm_awards_br_path, $typespess_path, $typespess_launch_server_path, $command_symbol)
 {
+    if ($message->guild->owner_id != $owner_id) return; //Only process commands from a guild that Taislin owns
     if (!$command_symbol) $command_symbol = '!s';
     
     if (str_starts_with($message->content, $command_symbol . ' ')) { //Add these as slash commands?
