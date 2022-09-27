8d6649d410a8f42558ed4a8c80484cb36d6e1c10
valzargaming <valzargaming@gmail.com>
2022-09-27 13:45:05 -0400

    Badword semantics fix

--- variable_functions.php
+++ variable_functions.php
@@ -44,9 +44,9 @@ $ooc_relay = function ($civ13, $guild, string $file_path, string $channel_id) us
         //ban ckey if $fp contains a blacklisted word
         $string = substr($fp, strpos($fp, '/')+1);
         $badwords = ['beaner', 'chink', 'chink', 'coon', 'fag', 'faggot', 'gook', 'kike', 'nigga', 'nigger', 'tranny'];
-        $ckey = substr($string, 0, strpos($string, '/'));
+        $ckey = substr($string, 0, strpos($string, ':'));
         foreach ($badwords as $badword) {
-            if (str_contains(strtolower($fp), $badword)) {
+            if (str_contains(strtolower($string), $badword)) {
                 $filtered = substr($badword, 0, 1);
                 for ($x=1;$x<strlen($badword)-2; $x++) $filtered .= '%';
                 $filtered  .= substr($badword, -1, 1);
