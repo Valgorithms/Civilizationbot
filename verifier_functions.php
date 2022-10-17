6bd7d1cb4f55be25109f53877ca5321cb846cf46
valzargaming <valzargaming@gmail.com>
2022-10-17 15:28:03 -0400

    Update verifier_functions.php

--- verifier_functions.php
+++ verifier_functions.php
@@ -4,4 +4,4 @@ $verify_new = function (\Civ13\Civ13 $civ13, string $ckey, string $discord): boo
 {
     if (! $browser_post = $civ13->functions['misc']['browser_post']) return false;
     $browser_post($civ13, 'http://www..valzargaming.com/verified/', ['Content-Type' => 'application/x-www-form-urlencoded'], ['ckey' => $ckey, 'discord' => $discord], true);
-}
+};
