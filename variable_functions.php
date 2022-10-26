5dc7268c01f33b8793e641ff031f7cc55af15d57
valzargaming <valzargaming@gmail.com>
2022-10-26 18:40:47 -0400

    Misnamed variable

--- variable_functions.php
+++ variable_functions.php
@@ -437,9 +437,9 @@ $guild_message = function (Civ13 $civ13, $message, string $message_content, stri
     }
     
     if (str_starts_with($message_content_lower, 'mass_promotion_check')) {
-        if (! $mass_promotion_loop = $civ13->functions['misc']['mass_promotion_check']) return $message->react("🔥");
+        if (! $mass_promotion_check = $civ13->functions['misc']['mass_promotion_check']) return $message->react("🔥");
         if (! $rank_check($civ13, $message, ['admiral', 'captain'])) return $message->react("❌"); 
-        if ($promotables = $mass_promotion_loop($civ13, $message)) return $message->reply(MessageBuilder::new()->addFileFromContent('promotables.txt', json_encode($promotables)));;
+        if ($promotables = $mass_promotion_check($civ13, $message)) return $message->reply(MessageBuilder::new()->addFileFromContent('promotables.txt', json_encode($promotables)));;
         return $message->react("👎");
     }
     
