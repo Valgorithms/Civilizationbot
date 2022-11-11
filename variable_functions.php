afef4f9df358737b86a1a8fd8ea68d25295df22d
valzargaming <valzargaming@gmail.com>
2022-11-11 18:12:53 -0500

    Better logic for ckey command

--- variable_functions.php
+++ variable_functions.php
@@ -954,9 +954,13 @@ $on_message = function (Civ13 $civ13, $message) use ($guild_message, $nomads_dis
     }
     if (str_starts_with($message_content_lower, 'ckey')) {
         $ckey = trim(str_replace(['<@!', '<@', '>', '.', '_', ' '], '', substr($message_content, strlen('ckey'))));
-        if (! $age = $civ13->getByondAge($civ13->getByondPage($ckey))) ;return $message->reply("`$ckey` does not exist");
+        if (is_numeric($ckey)) {
+            if (! $item = $civ13->verified->get('discord', $ckey)) return $message->reply("`$ckey` is not registered to any ckey");
+            if (! $age = $civ13->getByondAge($civ13->getByondPage($item['ss13']))) return $message->reply("`$ckey` does not exist");
+            return $message->reply("`{$item['ss13']}` is registered to <@{$item['discord']}> ($age)");
+        }
+        if (! $age = $civ13->getByondAge($civ13->getByondPage($ckey))) return $message->reply("`$ckey` does not exist");
         if ($item = $civ13->verified->get('ss13', $ckey)) return $message->reply("`{$item['ss13']}` is registered to <@{$item['discord']}> ($age)");
-        if ($item = $civ13->verified->get('discord', $ckey)) return $message->reply("`{$item['ss13']}` is registered to <@{$item['discord']}> ($age)");
         return $message->reply("`$ckey` is not registered to any discord id ($age)");
     }
     
