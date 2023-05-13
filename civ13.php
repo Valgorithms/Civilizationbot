0788477bb0836690cf8eb1a768182bea7762c257
Valithor Obsidion <valzargaming@gmail.com>
2023-05-13 11:49:24 -0400

    Update civ13.php

--- civ13.php
+++ civ13.php
@@ -997,6 +997,7 @@ class Civ13
         if ($this->webserver_online) $status = 'online';
         else $status = 'offline';
         if ($reported_status != $status) {
+            $channel->sendMessage("Webserver is now **{$status}**");
             $channel->name = "{$webserver_name}-{$status}";
             $channel->guild->channels->save($channel);
         }
