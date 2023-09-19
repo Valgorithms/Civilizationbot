0d07e5f7471ad4b295c26a97bea5f3eb6f8d5909
Valithor Obsidion <valzargaming@gmail.com>
2023-09-18 22:19:37 -0400

    Update webapi.php

--- webapi.php
+++ webapi.php
@@ -718,17 +718,23 @@ $webapi = new HttpServer($loop, function (ServerRequestInterface $request) use (
                     }
                     $message .= 'has started!';
                     if ($playercount_channel = $civ13->discord->getChannel($civ13->channel_ids[$server . '-playercount']));
-                        if ($existingCount = explode('-', $playercount_channel->name)[1]) {
-                            $existingCount = intval($existingCount);
-                            if ($existingCount === 0) $message .= " There are currently no players on the $server server.";
-                            elseif ($existingCount === 1) $message .= " There is currently 1 player on the $server server.";
-                            elseif ($existingCount > 1) {
+                    if ($existingCount = explode('-', $playercount_channel->name)[1]) {
+                        $existingCount = intval($existingCount);
+                        switch ($existingCount) {
+                            case 0:
+                                $message .= ' There are currently no players on the ' . ($upper ?? $server) . ' server.';
+                                break;
+                            case 1:
+                                $message .= ' There is currently 1 player on the ' . ($upper ?? $server) . ' server.';
+                                break;
+                            default:
                                 if (isset($civ13->role_ids['30+']) && $civ13->role_ids['30+'] && ($existingCount >= 30)) $message .= " <@&{$civ13->role_ids['30+']}>,";
                                 elseif (isset($civ13->role_ids['15+']) && $civ13->role_ids['15+'] && ($existingCount >= 15)) $message .= " <@&{$civ13->role_ids['15+']}>,";
                                 elseif (isset($civ13->role_ids['2+']) && $civ13->role_ids['2+'] && ($existingCount >= 2)) $message .= " <@&{$civ13->role_ids['2+']}>,";
-                                $message .= " There are currently $existingCount players on the $server server.";
-                            }
+                                $message .= ' There are currently ' . $existingCount . ' players on the ' . ($upper ?? $server) . ' server.';
+                                break;
                         }
+                    }
                     // A future update should include a way to call a $civ13 function using the server and round id
                     break;
                 case 'respawn_notice':
