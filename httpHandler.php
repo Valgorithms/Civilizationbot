a32ca55b3995cb08e24d1b5c3603661c0c1f2c07
Valithor Obsidion <valzargaming@gmail.com>
2023-09-21 14:26:31 -0400

    Update httpHandler.php

--- httpHandler.php
+++ httpHandler.php
@@ -105,8 +105,8 @@ class HttpHandler extends Handler implements HttpHandlerInterface
 
     public function processEndpoint(ServerRequestInterface $request): Response
     { // TODO
-        $params = $request->getQueryParams();
-        $data = json_decode($params['data'], true);
+        $data = $request->getQueryParams();
+        if ($data['data']) $data = json_decode($data['data'], true);
         $path = $request->getUri()->getPath();
         foreach ($this->handlers as $endpoint => $callback) {
             switch ($this->match_methods[$endpoint]) {
