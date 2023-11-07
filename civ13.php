d52cf444ef53a806138489b46884ae89f32885e2
Valithor Obsidion <valzargaming@gmail.com>
2023-11-06 23:17:15 -0500

    Update civ13.php

--- civ13.php
+++ civ13.php
@@ -4515,7 +4515,7 @@ class Civ13
     public function updateFilesFromMemberRoles(callable $callback, array $file_paths, array $required_roles): void
     {
         foreach ($file_paths as $file_path) {
-            if (!file_exists($this->files[$file_path]) || ! $file = @fopen($this->files[$file_path], 'a')) continue;
+            if (! file_exists($this->files[$file_path]) || ! $file = @fopen($this->files[$file_path], 'a')) continue;
             ftruncate($file, 0);
             $file_contents = '';
             foreach ($this->verified as $item) {
