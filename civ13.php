f9f9feef23639a9ce2bf06b9e9da719c7fc81a7d
Valithor Obsidion <valzargaming@gmail.com>
2023-11-20 00:41:46 -0500

    Update civ13.php

--- civ13.php
+++ civ13.php
@@ -3181,7 +3181,6 @@ class Civ13
         foreach ($removed as $item) $removed_items .= json_encode($item, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES) . PHP_EOL;
         if ($removed_items) $message = 'Removed from the verified list: ```json' . PHP_EOL . $removed_items . PHP_EOL . '```' . PHP_EOL . $message;
         if ($message) $this->logger->info($message);
-        else $this->logger->info("Removed from the verified list: $removed_items");
         return ['success' => true, 'message' => $message];
     }
     
