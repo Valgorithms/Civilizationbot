c4534ee0f79a196c010ac3f35806c114ee8af8e3
Valithor Obsidion <valzargaming@gmail.com>
2024-01-03 08:12:03 -0500

    Suppress verifier ping

--- civ13.php
+++ civ13.php
@@ -3716,7 +3716,7 @@ class Civ13
             : 'offline';
         if ($reported_status != $status) {
             $msg = "Verifier is now **{$status}**.";
-            if ($status === 'offline') $msg .= PHP_EOL . "Verifier technician <@{$this->technician_id}> has been notified.";
+            //if ($status === 'offline') $msg .= PHP_EOL . "Verifier technician <@{$this->technician_id}> has been notified.";
             $this->sendMessage($channel, $msg);
             $channel->name = "{$verifier_name}-{$status}";
             return $channel->guild->channels->save($channel);
