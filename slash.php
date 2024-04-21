b390b51344834eb94e1fce5b8b440b4cf816ce56
Valithor Obsidion <valzargaming@gmail.com>
2024-04-21 17:17:40 -0400

    Enable campaign

--- slash.php
+++ slash.php
@@ -600,8 +600,7 @@ class Slash
 
         $this->civ13->discord->listenCommand('join_campaign', function (Interaction $interaction): PromiseInterface
         {
-            return $interaction->respondWithMessage(MessageBuilder::new()->setContent('Factions are not ready to be assigned yet'), true);
-            //Disabled for now
+            //return $interaction->respondWithMessage(MessageBuilder::new()->setContent('Factions are not ready to be assigned yet'), true);
             if (! $this->civ13->getVerifiedItem($interaction->member->id)) return $interaction->respondWithMessage(MessageBuilder::new()->setContent("You are either not currently verified with a byond username or do not exist in the cache yet"), true);
             
             foreach ($interaction->member->roles as $role) if ($role->id === $this->civ13->role_ids['red'] || $role->id === $this->civ13->role_ids['blue']) return $interaction->respondWithMessage(MessageBuilder::new()->setContent("You're already in a faction!"), true);
