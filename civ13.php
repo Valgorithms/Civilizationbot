6ab7cf6932e92203e971d91f0db4ecbc1675ca42
Valithor Obsidion <valzargaming@gmail.com>
2024-05-07 07:05:45 -0400

    When code is updated, schedule restart in 5 mins

--- civ13.php
+++ civ13.php
@@ -1661,6 +1661,15 @@ class Civ13
                     $this->loop->addTimer(5, function () { execInBackground('git pull'); });
                 }
                 if (isset($this->channel_ids['staff_bot']) && $channel = $this->discord->getChannel($this->channel_ids['staff_bot'])) $this->sendMessage($channel, 'GitHub push event webhook received, updating code...');
+                if (isset($this->timers['update_pending']) && $this->timers['update_pending'] instanceof TimerInterface) {
+                    $this->loop->cancelTimer($this->timers['update_pending']);
+                    unset($this->timers['update_pending']);
+                }
+                $this->timers['update_pending'] = $this->loop->addTimer(300, function () {
+                    \restart();
+                    $this->discord->close();
+                    die();
+                });
                 return new HttpResponse(HttpResponse::STATUS_OK);
             }
             $headers = $request->getHeaders();
