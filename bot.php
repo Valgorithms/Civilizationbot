313aab763fdcfb56eec43e9ae29d9754edf41351
Valithor Obsidion <valzargaming@gmail.com>
2024-07-02 18:48:49 -0400

    Moderate IC chat for netspeak

--- bot.php
+++ bot.php
@@ -187,6 +187,39 @@ foreach (['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к'
     $options['ic_badwords'][] = $arr;
 }
 
+$exact_netspeak = [ // Words that are not necessarily bad, but are used in a way that is disruptive to the server
+    'omg',
+    'brb',
+    'g2g',
+    'lol',
+    'rofl',
+    'lmao',
+    'lmfao',
+    'kek',
+    'tldr',
+    'idk',
+
+    'skibidi',
+    'gyatt',
+    'rizz',
+    'sussy',
+    'sigma',
+    'goon',
+    'pog',
+    'poggers',
+    'cringe',
+    'uwu'
+];
+
+foreach ($exact_netspeak as $word) $options['ic_badwords'][] = [
+    'word' => $word,
+    'duration' => '1 week',
+    'reason' => 'You must speak with the proper lingo/vocabulary of the current epoch and setting you’re playing.',
+    'category' => 'netspeak',
+    'method' => 'exact',
+    'warnings' => 3
+];
+
 // Write editable configurations to a single JSON file
 /*
 $json = json_encode($options, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
