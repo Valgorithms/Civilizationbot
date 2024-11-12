7f8031e09598d50dc55ecb418c2d8615322803c6
Valithor Obsidion <valzargaming@gmail.com>
2024-11-12 18:28:15 -0500

    Update return type for status_changer_random

--- variable_functions.php
+++ variable_functions.php
@@ -8,31 +8,35 @@
 
 use Civ13\Civ13;
 use Discord\Parts\User\Activity;
+use React\Promise\PromiseInterface;
 
-$status_changer_random = function (Civ13 $civ13): bool
+use function React\Promise\reject;
+use function React\Promise\resolve;
+
+$status_changer_random = function (Civ13 $civ13): PromiseInterface
 { // on ready
     if (! $civ13::status) {
         unset($civ13->timers['status_changer_timer']);
-        $civ13->logger->warning('status is not defined');
-        return false;
+        $civ13->logger->warning($err = 'status is not defined');
+        return reject($err);
     }
     if (! $status_array = file($civ13::status, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES)) {
         unset($civ13->timers['status_changer_timer']);
-        $civ13->logger->warning('unable to open file `' . $civ13::status . '`');
-        return false;
+        $civ13->logger->warning($err = 'unable to open file `' . $civ13::status . '`');
+        return reject($err);
     }
     list($status, $type, $state) = explode('; ', $status_array[array_rand($status_array)]);
-    if (! $status) return false;
+    if (! $status) return reject('status is not defined');
     $activity = new Activity($civ13->discord, [ // Discord status            
         'name' => $status,
         'type' => (int) $type, // 0, 1, 2, 3, 4 | Game/Playing, Streaming, Listening, Watching, Custom Status
     ]);
     $civ13->statusChanger($activity, $state);
-    return true;
+    return resolve();
 };
 $status_changer_timer = function (Civ13 $civ13) use ($status_changer_random): void
 { // on ready
-    if (! isset($civ13->timers['status_changer_timer'])) $civ13->timers['status_changer_timer'] = $civ13->discord->getLoop()->addPeriodicTimer(120, function () use ($civ13, $status_changer_random) { $status_changer_random($civ13); });
+    if (! isset($civ13->timers['status_changer_timer'])) $civ13->timers['status_changer_timer'] = $civ13->discord->getLoop()->addPeriodicTimer(120, fn()=> $status_changer_random($civ13));
 };
 /*$on_ready = function (Civ13 $civ13): void
 {    
