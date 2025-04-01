6c7770ce4106169d91f40942e7a2bddc2671fc59
Valithor Obsidion <valzargaming@gmail.com>
2025-04-01 09:04:53 -0400

    ServerAPI helper functions

--- src/Civ14/ServerAPI.php
+++ src/Civ14/ServerAPI.php
@@ -2,6 +2,7 @@
 
 namespace Civ14;
 
+use Psr\Http\Message\ResponseInterface;
 use React\Http\Browser;
 use React\Promise\PromiseInterface;
 
@@ -19,6 +20,7 @@ class ServerAPI
     private string $protocol = 'http';
     private string $ip;
     private int|string $port = 1212;
+    private string $watchdogToken;
 
     public function __construct(
         protected $gameServer
@@ -36,8 +38,8 @@ class ServerAPI
      */
     public function getStatus(): PromiseInterface
     {
-        return $this->httpClient->get($this->baseURL() . '/status')
-            ->then(fn($response) => json_decode((string) $response->getBody(), true));
+        return $this->get($this->baseURL() . '/status')
+            ->then(fn(ResponseInterface $response) => self::parseResponse($response));
     }
 
     /**
@@ -47,8 +49,8 @@ class ServerAPI
      */
     public function getInfo(): PromiseInterface
     {
-        return $this->httpClient->get($this->baseURL() . '/info')
-            ->then(fn($response) => json_decode((string) $response->getBody(), true));
+        return $this->get($this->baseURL() . '/info')
+            ->then(fn(ResponseInterface $response) => self::parseResponse($response));
     }
 
     /**
@@ -59,10 +61,35 @@ class ServerAPI
     public function shutdown(): PromiseInterface
     {
         return $this->httpClient->post($this->baseURL() . '/shutdown', [
-            'headers' => $this->getAuthHeaders(),
-        ])->then(fn($response) => $response->getStatusCode() === 200);
+            'headers' => $this->authHeaders(),
+        ])->then(fn(ResponseInterface $response) => self::isResponseSuccessful($response));
     }
 
+    /**
+     * Sends a GET request to the specified URL with optional headers.
+     *
+     * @param string $url The URL to send the GET request to.
+     * @param array $headers An optional array of headers to include in the request.
+     * @return PromiseInterface A promise representing the asynchronous HTTP response.
+     */
+    public function get($url, array $headers = array()): PromiseInterface
+    {
+        return $this->httpClient->get($url, $headers);
+    }
+
+    /**
+     * Sends a POST request to the specified URL with the given headers and body.
+     *
+     * @param string $url The URL to send the POST request to.
+     * @param array $headers An associative array of headers to include in the request.
+     * @param string $body The body content to include in the POST request. Defaults to an empty string.
+     * @return PromiseInterface A promise representing the asynchronous HTTP response.
+     */
+    public function post($url, array $headers = array(), $body = ''): PromiseInterface
+    {
+        return $this->httpClient->post($url, $headers, $body);
+    }
+    
     /**
      * Notify the server of an available update (requires authorization).
      *
@@ -71,8 +98,44 @@ class ServerAPI
     public function update(): PromiseInterface
     {
         return $this->httpClient->post($this->baseURL() . '/update', [
-            'headers' => $this->getAuthHeaders(),
-        ])->then(fn($response) => $response->getStatusCode() === 200);
+            'headers' => $this->authHeaders(),
+        ])->then(fn(ResponseInterface $response) => self::isResponseSuccessful($response));
+    }
+
+    /**
+     * Parses the given HTTP response and decodes its JSON content into an associative array.
+     *
+     * @param ResponseInterface $response The HTTP response to parse.
+     * 
+     * @return array The decoded JSON content as an associative array.
+     */
+    public static function parseResponse(ResponseInterface $response): array
+    {
+        return json_decode($response->getBody()->getContents(), true);
+    }
+
+    /**
+     * Determines if the given HTTP response is successful.
+     *
+     * This method checks if the status code of the provided response
+     * is equal to 200, which indicates a successful HTTP request.
+     *
+     * @param ResponseInterface $response The HTTP response to evaluate.
+     * @return bool True if the response status code is 200, otherwise false.
+     */
+    public static function isResponseSuccessful(ResponseInterface $response)
+    {
+        return $response->getStatusCode() === 200;
+    }
+
+    /**
+     * Constructs the base URL for the server using the protocol, IP address, and port.
+     *
+     * @return string The complete base URL in the format: protocol://ip:port
+     */
+    public function baseUrl(): string
+    {
+        return $this->protocol . '://' . $this->ip . ':' . $this->port;
     }
 
     /**
@@ -80,9 +143,9 @@ class ServerAPI
      *
      * @return array
      */
-    private function getAuthHeaders(): array
+    public function authHeaders(): array
     {
-        $watchdogToken = $this->gameServer->civ13->watchdogToken ?? null;
+        $watchdogToken = $this->watchdogToken ?? null;
         return $watchdogToken ? ['WatchdogToken' => $watchdogToken] : [];
     }
 
@@ -101,8 +164,8 @@ class ServerAPI
         $this->port = $port;
     }
 
-    protected function baseUrl(): string
+    public function setWatchdogToken(string $token): void
     {
-        return $this->protocol . '://' . $this->ip . ':' . $this->port;
+        $this->watchdogToken = $token;
     }
 }
