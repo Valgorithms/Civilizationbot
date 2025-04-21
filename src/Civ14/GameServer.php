4e8c371031d2bb0d52f3901a53d0e9940effa202
Valithor Obsidion <valzargaming@gmail.com>
2025-04-21 01:09:29 -0400

    Set playing to 0 if offline

--- src/Civ14/GameServer.php
+++ src/Civ14/GameServer.php
@@ -88,6 +88,7 @@ class GameServer
     public function playercountTimer(): TimerInterface
     {
         if (is_resource($socket = @fsockopen('localhost', $this->port, $errno, $errstr, 1))) $this->getStatus();
+        else $this->playing = 0;
         if (! isset($this->timers['playercount_timer]'])) $this->timers['playercount_timer'] = $this->loop->addPeriodicTimer(600, fn () => $this->playercountChannelUpdate($this->playing));
         return $this->timers['playercount_timer'];
     }
