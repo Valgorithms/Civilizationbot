bcbd2d791c60ebc7fbe5710bdf11276c32b6756a
Cees-Jan Kiewiet <github@ceesjankiewiet.nl>
2025-04-26 16:47:09 +0200

    The loop has already started at this point, attempting to start it again will cause issues

--- src/Civ13/Civ13.php
+++ src/Civ13/Civ13.php
@@ -748,7 +748,7 @@ class Civ13
         }
         $this->logger->info('Starting Discord loop');
         if (!(isset($this->discord))) $this->logger->warning('Discord not set!');
-        else $this->discord->run();
+//        else $this->discord->run();
     }
     /**
      * Stops the bot and logs the shutdown message.
