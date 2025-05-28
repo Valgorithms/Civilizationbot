96add83dd5a799ad48492f640a1478d095d64a7d
Valithor Obsidion <valzargaming@gmail.com>
2025-05-28 09:37:22 -0400

    Abstract SS14Medals logic

--- src/Civ13/MessageCommand/Commands/SS14Medals.php
+++ src/Civ13/MessageCommand/Commands/SS14Medals.php
@@ -11,6 +11,7 @@ use Civ13\Civ13;
 use Civ13\MessageCommand\Civ13MessageCommand;
 use Civ14\GameServer;
 use Discord\Parts\Channel\Message;
+use Discord\Parts\Guild\Guild;
 use React\Promise\PromiseInterface;
     
 enum SS14MedalEmojis: string
@@ -64,19 +65,57 @@ class SS14Medals extends Civ13MessageCommand
             if (! $item = $this->civ13->ss14verifier->get('discord', $id)) return $this->civ13->reply($message, "Unable to locate verified Discord account with id `$id`.");
             $id = $item['ss14'] ?? null;
         }
-        if (! $item = $this->gameserver->medals->get('user', $id)) return $this->civ13->reply( $message,
-            "No SS14 medals found for `$id`."
-        );
-
-        if ($guild = $this->discord->guilds->get('id', $this->civ13->civ13_guild_id))
-            foreach ($item['medals'] as &$medal)
-                if ($enum = SS14MedalEmojis::fromName($medal))
-                    if ($emoji = $guild->emojis->get('name', $enum->emoji()))
-                        $medal = $emoji . " $medal";
-
+        if (! $medals = self::getMedals($this->gameserver, $id)) return $this->civ13->reply( $message, "No SS14 medals found for `$id`.");
         return $this->civ13->reply($message,
-            "Medals for {$item['user']}:" . PHP_EOL
-            . (implode(PHP_EOL, $item['medals']))
+            "Medals for $id:" . PHP_EOL
+            . (implode(PHP_EOL, ($guild = $this->discord->guilds->get('id', $this->civ13->civ13_guild_id))
+                ? self::getMedalsWithEmojis($guild, $medals)
+                : $medals))
         );
+    }
+
+    /**
+     * Appends emojis to the beginning of each medal.
+     *
+     * @param String[] $medals
+     * @return array
+     */
+    public static function getMedalsWithEmojis(Guild $guild, array $medals): array
+    {
+        return array_map(function($medal) use ($guild) {
+            if ($enum = SS14MedalEmojis::fromName($medal))
+                if ($emoji = $guild->emojis->get('name', $enum->emoji()))
+                    return $emoji . " $medal";
+            return $medal;
+        }, $medals);
+    }
+
+    /**
+     * Retrieves the emojis for the given medals.
+     *
+     * @param String[] $medals
+     * @return array
+     */
+    public static function getMedalEmojis(Guild $guild, array $medals): array
+    {
+        foreach ($medals as &$medal)
+            if ($enum = SS14MedalEmojis::fromName($medal))
+                $medal = ($emoji = $guild->emojis->get('name', $enum->emoji()))
+                    ? $emoji
+                    : null;
+        return array_filter($medals);
+    }
+    
+    /**
+     * Retrieves the medals associated with a specific SS14 user.
+     *
+     * @param string $ss14
+     * @return array|null
+     */
+    public static function getMedals(GameServer $gameserver, string $ss14): ?array
+    {
+        return ($item = $gameserver->medals->get('user', $ss14))
+            ? $item['medals'] ?? null
+            : null;
     }
 }
