a54997c772084f5d1d80d15c9047c1c167e5d528
Valithor Obsidion <valithor@discordphp.org>
2026-06-12 12:50:23 -0400

    Create ListVerified.php

---
+++ src/Civ13/MessageCommand/Commands/ListVerified.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is a part of the Civilizationbot project.
+ *
+ * Copyright (c) 2021-present Valithor Obsidion <valithor@civ13.org>
+ *
+ * This file is subject to the MIT license that is bundled
+ * with this source code in the LICENSE.md file.
+ */
+
+namespace Civ13\MessageCommand\Commands;
+
+use Civ13\Civ13;
+use Civ13\MessageCommand\Civ13MessageCommand;
+use Discord\Parts\Channel\Message;
+use React\Promise\PromiseInterface;
+
+/**
+ * Handles the "listverified" command.
+ */
+class ListVerified extends Civ13MessageCommand
+{
+    public function __invoke(Message $message, string $command, array $message_filtered): PromiseInterface
+    {
+        return $message->reply(Civ13::createBuilder()
+            ->setContent('Verified Users')
+            ->addFileFromContent(
+                'verified.json',
+                json_encode($this->civ13->verifier->getVerified()->jsonSerialize(), JSON_PRETTY_PRINT)
+            ));
+    }
+}
